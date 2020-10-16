fa6278a9db803ded9577fdc1ae57b1cf3b46f87d
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-16 13:46:08 +0530

    Added HinduSpider

--- Scrapers/core.py
+++ Scrapers/core.py
@@ -1,8 +1,9 @@
 from spiders.NDTVSpider import NDTVSpider
-
+from spiders.HinduSpider import HinduSpider
 
 from scrapy.crawler import CrawlerProcess
 
 process = CrawlerProcess()
 process.crawl(NDTVSpider)
+process.crawl(HinduSpider)
 process.start()
--- Scrapers/spiders/NDTVSpider.py
+++ Scrapers/spiders/NDTVSpider.py
@@ -7,12 +7,11 @@ class NDTVSpider(scrapy.Spider):
     def parse(self,response):
 
         all_headlines = response.css('div.new_storylising_contentwrap')
-        for header in all_headlines:
-            header_text = header.css('h2.nstory_header a::attr(title)').extract()
-            header_link = header.css('h2.nstory_header a::attr(href)').extract()
-            description = header.css('div.nstory_intro::text').extract()
+        for i in range(10):
+            header_text = all_headlines[i].css('h2.nstory_header a::attr(title)').extract()
+            header_link = all_headlines[i].css('h2.nstory_header a::attr(href)').extract()
+            description = all_headlines[i].css('div.nstory_intro::text').extract()
             yield {
                 'text': header_text,
                 'link': header_link,
-                'desc': description
             }

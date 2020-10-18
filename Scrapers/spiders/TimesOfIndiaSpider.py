e37f9e29dc3d414b4374cb00d9c482d925f0e81a
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-18 20:16:45 +0530

    Added TimesofIndia spider

---
+++ Scrapers/spiders/TimesOfIndiaSpider.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class TimesOfIndiaSpider(scrapy.Spider):
+
+    name = "timesofindia"
+    start_urls = ["https://timesofindia.indiatimes.com/briefs"]
+
+    def parse(self, response):
+        all_headlines = response.css('div.brief_box')
+
+        for i in range(10):
+
+            headline_text = all_headlines[i].css('h2 a::text').extract_first()
+            headline_link = "https://timesofindia.indiatimes.com" + str(all_headlines[i].css('h2 a::attr(href)').extract_first())
+            
+            yield {
+                'text': headline_text,
+                'link': headline_link
+            }
+        

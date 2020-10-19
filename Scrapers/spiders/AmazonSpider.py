d189bba050332eccc176caf7861ee6aff76e7261
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-19 13:46:23 +0530

    Updated AmazonSpider

--- Scrapers/spiders/AmazonSpider.py
+++ Scrapers/spiders/AmazonSpider.py
@@ -13,12 +13,16 @@ class AmazonSpider(scrapy.Spider):
 
         all_products_names = respose.css('.a-color-base.a-text-normal::text').extract()
         all_products_links = respose.css('.a-link-normal.a-text-normal::attr(href)').extract()
+        all_products_prices = respose.css('.a-price-whole::text').extract()
+
         for i in range(5):
             product_name = all_products_names[i]
+            product_price = all_products_prices[i]
             product_link = "https://www.amazon.in" + str(all_products_links[i])
-            
+
             yield {
                 "product_name": product_name,
+                "product_price":product_price,
                 "product_link": product_link
             }
 

b2f9f8ae10038dbad025fd1fe0680a1a57052442
sooryaprakash31 <sooryaprakash.r31@gmail.com>
2020-10-24 20:01:48 +0530

    Added Flipkart Spider

---
+++ Scrapers/spiders/FlipkartSpider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+class FlipkartSpider(scrapy.Spider):
+    
+    name = "flipkart"
+    start_urls = ["https://www.flipkart.com/search?q="]
+
+    def __init__(self, product):
+        product = product.replace(" ","+")
+        FlipkartSpider.start_urls = ["https://www.flipkart.com/search?q="+product]
+
+    def parse(self, response):
+
+        all_products_names = response.css('._3wU53n::text').extract()
+        all_products_prices = response.css('._2rQ-NK::text').extract()
+        all_products_links = response.css('._31qSD5::attr(href)').extract()
+
+        for i in range(10):
+
+            product_name = all_products_names[i]
+            product_price = all_products_prices[i]
+            product_link = "https://www.flipkart.com" +all_products_links[i]
+        
+            yield {
+                "product_name":product_name,
+                "product_price": product_price,
+                "product_link": product_link
+            }
